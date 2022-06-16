7084093cdb4444f5273ea697ec9b4391f7393133
Jardiano Conceição Batista de Almeida <jardiano@MacBook-Pro-de-Jardiano.local>
2022-06-16 20:35:02 -0300

    ensure

---
+++ test/domain/usecases/remote_autentication_test.dart
@@ -0,0 +1,37 @@
+
+import 'package:faker/faker.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:mockito/annotations.dart';
+import 'package:mockito/mockito.dart';
+
+import 'remote_autentication_test.mocks.dart';
+
+class RemoteAutentication {
+  final  HttpClient httpClient;
+  final String url;
+
+  RemoteAutentication({required this.httpClient, required this.url});
+
+  @override
+  Future<void> auth( ) async {
+    httpClient.request(url: url);
+  }
+}
+
+abstract class HttpClient {
+  Future<void> request({required String url});
+}
+
+
+@GenerateMocks([HttpClient])
+void main() {
+  test('Should call HttpClient with correct values', () async {
+    final httpClient = MockHttpClient();
+    final url = faker.internet.httpUrl();
+    final sut = RemoteAutentication(httpClient: httpClient, url: url);
+
+    await sut.auth();
+
+    verify(httpClient.request(url: url));
+  });
+}
